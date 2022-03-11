5a247d062c64eb16f11faba764d7ec5ff2e81031
Mamta <mamta16@nmsu.edu>
2022-03-11 14:39:10 -0700

    created two pages

--- src/LTISourceOmnidirectional/listener.jl
+++ src/LTISourceOmnidirectional/listener.jl
@@ -1,11 +1,11 @@
-struct stationaryListenerOmnidirectional
+struct LTIListener
    sourceList::Vector{Source}
    position::Vector{Float64}
 end
 
-function (listener::stationaryListenerOmnidirectional)(t₀::Float64)
-sourceList = listener.sourceList
-𝐩ᵣ = listener.position
+function (Ψ::LTIListener)(t₀::Float64)
+sourceList = Ψ.sourceList
+𝐩ᵣ = Ψ.position
    val = 0.0
    for i = 1:length(sourceList)
       val+=sourceList[i](𝐩ᵣ,t₀)
